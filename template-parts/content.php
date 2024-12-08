0c65baf05fc7ac9e803f5284bdfe64bc69ddb66b
kundan <kundanbora77@gmail.com>
2024-12-08 16:02:29 +0530

    updated post content page

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,27 +12,32 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+//		if ( is_singular() ) :
+//			the_title( '<h1 class="entry-title">', '</h1>' );
+//		else :
+//			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+//		endif;
 
-		if ( 'post' === get_post_type() ) :
+//		if ( 'post' === get_post_type() ) :
 			?>
 			<div class="entry-meta">
 				<?php
-				portfolio_posted_on();
-				portfolio_posted_by();
+//				portfolio_posted_on();
+//				portfolio_posted_by();
 				?>
 			</div><!-- .entry-meta -->
-		<?php endif; ?>
+<!--		--><?php //endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php portfolio_post_thumbnail(); ?>
+<!--	--><?php //portfolio_post_thumbnail(); ?>
 
 	<div class="entry-content">
-		<?php
+
+        <div class="container mt-5 mx-auto p-6 bg-white dark:bg-gray-800 dark:text-white rounded-lg shadow-lg md:flex md:space-x-10 max-w-7xl">
+            <div class="flex flex-col justify-between text-center md:text-left space-y-4 md:space-y-6">
+        <?php
+
+
 		the_content(
 			sprintf(
 				wp_kses(
@@ -48,16 +53,26 @@
 			)
 		);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'portfolio' ),
-				'after'  => '</div>',
-			)
-		);
+//		wp_link_pages(
+//			array(
+//				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'portfolio' ),
+//				'after'  => '</div>',
+//			)
+//		);
 		?>
-	</div><!-- .entry-content -->
+            </div>
+        </div>
+	</div>
+
+    <!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php portfolio_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+<!--		--><?php //portfolio_entry_footer(); ?>
+	</footer>
+
+    <!-- .entry-footer -->
+</article>
+
+<!-- #post-
+<?php //the_ID(); ?>
+-->
